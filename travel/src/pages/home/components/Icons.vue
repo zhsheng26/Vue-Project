3525387dfd1a7a915d71634bb004481479a6bcc4
zhangsheng <zhsheng26@gmail.com>
2019-01-05 23:25:05 +0800

    pages

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -1,26 +1,52 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img-container">
-                <img class="icon-img-content" src="">
-            </div>
-            <p class="icon-desc">热门景点</p>
-        </div>
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" v-bind:key="index">
+                <div class="icon" v-for="item in page" v-bind:key="item.id">
+                    <div class="icon-img-container">
+                        <img class="icon-img-content" :src="item.imgUrl">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 
 </template>
 
 <script>
+    import 'swiper/dist/css/swiper.css'
+    import {swiper, swiperSlide} from 'vue-awesome-swiper'
+
     export default {
         name: "Icons",
         props: {
             list: Array,
+        },
+        computed: {
+            pages() {
+                const pages = [];
+                this.list.forEach((item, index) => {
+                    const page = Math.floor(index / 8)
+                    if (!pages[page]) {
+                        pages[page] = []
+                    }
+                    pages[page].push(item)
+
+                });
+                return pages;
+            }
+        },
+        components: {
+            swiper,
+            swiperSlide
         }
     }
 </script>
 
 <style lang="stylus" scoped>
     @import "~styles/varibles.styl"
+    @import "~styles/mixins.styl"
     .icons
         overflow hidden
         height 0
@@ -38,7 +64,6 @@
                 left 0
                 right 0
                 bottom .44rem
-                background #1abc9c
                 box-sizing border-box
                 padding .1rem
                 .icon-img-content
@@ -54,5 +79,6 @@
                 line-height .44rem
                 color $darkTextColor
                 text-align center
+                ellipsis()
 
 </style>
